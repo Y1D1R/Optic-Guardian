77388ce868e98f4882cee70136f13f05cdf3d7fd
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-10-21 20:49:29 +0200

    feat(entity): add DataIngestionConfig

---
+++ src/RetinoNet/entity/config_entity.py
@@ -0,0 +1,10 @@
+# src/RetinoNet/entity/config_entity.py
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
