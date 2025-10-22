f6dfef976b1398720a9c7a55ba33e50e4e3f0837
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-10-22 13:34:55 +0200

    feat(config): add model base preparation loader

--- src/RetinoNet/config/configuration.py
+++ src/RetinoNet/config/configuration.py
@@ -4,7 +4,7 @@ import os
 
 from RetinoNet.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from RetinoNet.utils.common import read_yaml, create_directories
-from RetinoNet.entity.config_entity import DataIngestionConfig
+from RetinoNet.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 from RetinoNet import logger
 from dotenv import load_dotenv
 
@@ -37,11 +37,30 @@ class ConfigurationManager:
         local_data_file = Path(cfg.local_data_file)
         unzip_dir = Path(cfg.unzip_dir)
 
-        di = DataIngestionConfig(
+        data = DataIngestionConfig(
             root_dir=root_dir,
             source_URL=str(cfg.source_URL),
             local_data_file=local_data_file,
             unzip_dir=unzip_dir,
         )
         log.info("data_ingestion config built")
-        return di
+        return data
+    
+    # ---------------- Prepare Base Model ----------------
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([Path(config.root_dir)])
+
+        base_model = PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path = Path(config.base_model_path),
+            updated_base_model_path = Path(config.updated_base_model_path),
+            params_image_size = self.params.IMAGE_SIZE,
+            params_learning_rate = self.params.LEARNING_RATE,
+            params_include_top = self.params.INCLUDE_TOP,
+            params_weights = self.params.WEIGHTS,
+            params_classes = self.params.CLASSES,
+            model_name = self.params.MODEL_NAME
+        )
+        log.info("Base model config built")
+        return base_model
