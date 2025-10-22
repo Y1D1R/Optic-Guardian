daf42e953fdd73ee9d6bfeda27a39cd03c8f7dfd
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-10-22 14:34:58 +0200

    feat(main): add entrypoint for stage_02_prepare_base_model

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from RetinoNet import logger # type: ignore
 from RetinoNet import get_logger
 from RetinoNet.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from RetinoNet.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "01 - Data Ingestion stage"
 log = get_logger(__name__)
 try:
     log.info(">>>>>> stage %s started <<<<<<", STAGE_NAME)
     DataIngestionTrainingPipeline().main()
+    log.info(">>>>>> stage %s completed <<<<<<\n\nx==========x", STAGE_NAME)
+except Exception:
+    log.exception("stage failed")
+    raise
+
+STAGE_NAME = "02 - Prepare Base Model stage"
+log = get_logger(__name__)
+try:
+    log.info(">>>>>> stage %s started <<<<<<", STAGE_NAME)
+    PrepareBaseModelTrainingPipeline().main()
     log.info(">>>>>> stage %s completed <<<<<<\n\nx==========x", STAGE_NAME)
 except Exception:
     log.exception("stage failed")
