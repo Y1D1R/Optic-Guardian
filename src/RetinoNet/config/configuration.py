4e9193b9ae6ecb2ae51ca9e4cd21f9571396ec09
y1d1r <koulalyidhiraghiles@gmail.com>
2025-10-26 22:10:03 +0100

    feat(config): add model training loader

--- src/RetinoNet/config/configuration.py
+++ src/RetinoNet/config/configuration.py
@@ -4,7 +4,7 @@ import os
 
 from RetinoNet.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from RetinoNet.utils.common import read_yaml, create_directories
-from RetinoNet.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from RetinoNet.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, ModelTrainingConfig
 from RetinoNet import logger
 from dotenv import load_dotenv
 
@@ -64,3 +64,32 @@ class ConfigurationManager:
         )
         log.info("Base model config built")
         return base_model
+
+    # ---------------- Model Training ----------------
+    def model_training_config(self) -> ModelTrainingConfig:
+        model_training = self.config.model_training
+        prepare_base_model_config = self.config.prepare_base_model
+        params = self.params
+        training_data = Path(self.config.data_ingestion.unzip_dir)
+
+    
+        create_directories([
+            Path(model_training.root_dir),
+            Path(model_training.trained_model_path).parent,
+        ])
+        
+        model_training_config = ModelTrainingConfig(
+            root_dir=Path(model_training.root_dir),
+            trained_model_path=Path(model_training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model_config.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE,
+            params_learning_rate=params.LEARNING_RATE,
+            model_name=params.MODEL_NAME,
+            artifacts_dir=params.ARTIFACTS_DIR
+        )
+
+        return model_training_config
