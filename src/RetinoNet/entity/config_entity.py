2d4f5cf55e746c9fd6f2c0dab6695b08bffdd74f
y1d1r <koulalyidhiraghiles@gmail.com>
2025-10-26 22:08:31 +0100

    feat(config): add ModelTrainingConfig

--- src/RetinoNet/entity/config_entity.py
+++ src/RetinoNet/entity/config_entity.py
@@ -20,3 +20,18 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+@dataclass(frozen=True )
+class ModelTrainingConfig:
+    root_dir: Path
+    model_name: str
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+    params_learning_rate: float
+    artifacts_dir: str
+    
